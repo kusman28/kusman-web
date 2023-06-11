de5aa86604d089b2daa440ed829bee109ecd2c91
Khalid Usman <kusman758@gmail.com>
2023-06-11 23:55:24 +0800

    Init nuxt app.

---
+++ components/HeroComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+        <p>Test 123</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- <Tutorial/> -->
+  <HeroComponent/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
